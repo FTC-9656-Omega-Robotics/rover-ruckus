fd602884e88e0901fe8d7c9114b70506ca501c87
nidhirg <nidhirg2014@gmail.com>
2019-06-28 01:27:16 -0700

    started 11115 (gluten free) pure pursuit tutorial before realizing it needs localization to set up. will finish this later at a more reasonable time

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+//pure pursuit tutorial opmode
+
+public class MyOpMode extends OpMode {
+    @Override
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMovement.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+//pure pursuit tutorial movement class
+
+public class RobotMovement {
+    public static void goToPosition(double x, double y, double moveSpeed){
+        //double absoluteAngleToTarget = Math.atan2();
+    }
+}
